7fe6a3fe9174cf6c509dc5a370e600f8c0ffa654
TACAMP <112141419+TACAMP@users.noreply.github.com>
2022-11-30 18:55:29 +0900

    add ap.js

---
+++ app.js
@@ -0,0 +1,36 @@
+let hello = "Hello World!"
+let int1 = 1;
+let int2 = -10;
+let float1 = 3.14195
+let str1 = 'SHHisが熱いですよ！プロデューサーさん！！！！'
+let sum1 = 1+1
+let sum2 = 1-1
+let sum3 = 1*3
+let sum4 = 10/5
+let sum5 = 9%4
+let max = 100
+let min = 1
+let count = 0
+let i;
+let num = 0;
+
+// if (sum3 == 1) {
+//     alert(str1);
+// } else if (int2 == -1) {
+//     alert(hello);
+// } else {
+//     alert(float1);
+// } 
+
+// while (min < max) {
+//     min = min*2;
+//     count = count+1;
+// }
+
+// alert('２をかけ続けて' + max + 'になるまで' + count + '回です。');
+
+for (i=1;i<11;i++){
+    num = num + i;
+}
+
+alert('1から10まで1ずつ足し続けると' + num )
